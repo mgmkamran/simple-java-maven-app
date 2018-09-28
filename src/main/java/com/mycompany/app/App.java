5b8020047542d7a6c95c2a648d22eb56fcd19164
mgmkamran <mgmkamran@gmail.com>
2018-09-28 22:21:50 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World, first edit post commit!";
+    private final String message = "Hello World, first edit post commit.!";
 
     public App() {}
 
